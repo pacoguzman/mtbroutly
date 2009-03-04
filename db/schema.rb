c428087e72fc73a425a00e0bdfedb170d8d98006
pacoguzman <pacoguzmanp@gmail.com>
2009-03-04 16:07:38 -0500

    Installed and Migrated acts_as_taggable on_steroids plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090304210447) do
+ActiveRecord::Schema.define(:version => 20090304210702) do
 
   create_table "abuses", :force => true do |t|
     t.string   "email"
@@ -40,5 +40,19 @@ ActiveRecord::Schema.define(:version => 20090304210447) do
   end
 
   add_index "ratings", ["rateable_id", "rating"], :name => "index_ratings_on_rateable_id_and_rating"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
 end
