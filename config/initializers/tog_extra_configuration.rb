619255e0a1eb43db05c07e703ace6334aa639ad3
Paco Guzmán <pacoguzmanp@gmail.com>
2009-04-04 09:45:47 +0200

    Added extra tog configuration initializer

---
+++ config/initializers/tog_extra_configuration.rb
@@ -0,0 +1,3 @@
+# Configuración añadida sobre la configración TOG para personalizar el sitio web
+Tog::Interface.sections(:site).add "Routes", "/routes"
+Tog::Interface.sections(:member).add "Routes", "/member/routes"
