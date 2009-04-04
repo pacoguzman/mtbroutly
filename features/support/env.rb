333164d34fea8dcbab5d5abe88f478718d5daea3
Paco Guzmán <pacoguzmanp@gmail.com>
2009-04-04 10:59:57 +0200

    Structured features support as 84394 gist forked from balinterdi

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,19 @@
-# Sets up the Rails environment for Cucumber
 ENV["RAILS_ENV"] ||= "test"
+
+# Sets up the Rails environment for Cucumber
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
-Cucumber::Rails.use_transactional_fixtures
-Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling 
+
+require 'cucumber/formatters/unicode'  # Comment out this line if you don't want Cucumber Unicode support
+# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
+require 'cucumber/rails/rspec'
+require 'webrat/core/matchers'
+
+Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling
                               # (e.g. rescue_action_in_public / rescue_responses / rescue_from)
 
 require 'webrat'
-
 Webrat.configure do |config|
   config.mode = :rails
 end
@@ -27,5 +32,4 @@ MundoPepino::ModelsToClean = [
   # Orchard, Terrace, Crop...
 ]
 
-require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
+
--- features/support/javascript.rb
+++ features/support/javascript.rb
@@ -1,35 +1,21 @@
-# Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] ||= "selenium"
-require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
-#Cucumber::Rails.use_transactional_fixtures # Turn off
-Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling
-# (e.g. rescue_action_in_public / rescue_responses / rescue_from)
-
+# see http://wiki.github.com/aslakhellesoy/cucumber/setting-up-selenium for help
+require 'spec/expectations'
+require 'selenium'
 require 'webrat'
 
 Webrat.configure do |config|
   config.mode = :selenium
+  # Selenium defaults to using the selenium environment. Use the following to override this.
+  #config.application_environment = :test
 end
 
-require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
 
-require 'factory_girl'
-require File.expand_path(File.dirname(__FILE__) + '/../../spec/factories')
-
-require 'mundo_pepino'
-
-MundoPepino::ModelsToClean = [
-  Waypoint, # (TODO: quitar la coma final si es el primer modelo)
-  Route, # (TODO: quitar la coma final si es el primer modelo)
-  User
-  # MODELOS PARA LIMPIAR antes de cada escenario,
-  # por ejemplo:
-  # Orchard, Terrace, Crop...
-]
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
 
 Before do
-  MundoPepino::ModelsToClean.each { |model| model.destroy_all }
+  # truncating the database tables here, since transactional fixtures
+  # can not be used with selenium
+  DatabaseCleaner.clean
 end
+
