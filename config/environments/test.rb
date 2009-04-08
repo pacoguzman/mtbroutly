9e3b9ca1a198de94e847ebaa47b4ff823eb0b40e
Paco Guzmán <pacoguzmanp@gmail.com>
2009-04-08 10:39:07 +0200

    Config environment test for usign webrat in integration test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,7 +26,7 @@ config.action_mailer.delivery_method = :test
 config.gem 'rspec-rails', :lib => false, :version => '>=1.2.2'
 config.gem 'rspec', :lib => false, :version => '>=1.2.2'
 config.gem 'cucumber', :lib => false, :version => '>=0.2.2'
-config.gem 'webrat', :lib => false, :version => '>=0.4.3'
+config.gem 'webrat', :version => '>=0.4.3'
 config.gem "thoughtbot-shoulda", :lib => false, :source => "http://gems.github.com", :version => '>=2.10.1'
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com", :version => '>=1.2.0'
 config.gem "bmabey-database_cleaner", :lib => "database_cleaner", :source => "http://gems.github.com", :version => '>=0.1.2'
