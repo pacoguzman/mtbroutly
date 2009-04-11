f94f49be9df48ee49a56077567e7cae5c77fdd8a
Paco Guzmán <pacoguzmanp@gmail.com>
2009-04-11 21:12:19 +0200

    First step for assets_packager tasks: moved tog_core stylesheets and javascripts to default folder and modified layouts, and helpers. Pending set specific assests

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,13 +4,13 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   
-#  #TODO el meta language en función del lenguaje de la página
-#  #TODO los valores por defecto que hagan referencia al nombre final de la aplicación
-#  meta  :title        => "MTBRoutes",
-#    :description  => "MTBRoutes community",
-#    :keywords     => "mtb, routes, route, mountain bike, bike, bikes",
-#    :author => "Riding to NoWhere",
-#    :language => "en"
+  #  #TODO el meta language en función del lenguaje de la página
+  #  #TODO los valores por defecto que hagan referencia al nombre final de la aplicación
+  #  meta  :title        => "MTBRoutes",
+  #    :description  => "MTBRoutes community",
+  #    :keywords     => "mtb, routes, route, mountain bike, bike, bikes",
+  #    :author => "Riding to NoWhere",
+  #    :language => "en"
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -19,5 +19,13 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  # filter_parameter_logging :password  
+
+  # Overloading tog_core application controller. Now use assets_packager tasks
+  def set_javascripts_and_stylesheets
+    @javascripts = %w()
+    @stylesheets = %w()
+    @feeds = %w()
+  end
+
 end
