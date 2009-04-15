27bcafd01598891e71131f7e4b9c521ff4925523
Paco Guzmán <pacoguzmanp@gmail.com>
2009-04-15 22:57:03 +0200

    Updating cucumber configuration files

--- features/step_definitions/webrat_steps.rb
+++ features/step_definitions/webrat_steps.rb
@@ -20,25 +20,25 @@ When /^I follow "([^\"]*)"$/ do |link|
 end
 
 When /^I fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
-  fill_in(field, :with => value) 
+  fill_in(field, :with => value)
 end
 
 When /^I select "([^\"]*)" from "([^\"]*)"$/ do |value, field|
-  select(value, :from => field) 
+  select(value, :from => field)
 end
 
 # Use this step in conjunction with Rail's datetime_select helper. For example:
-# When I select "December 25, 2008 10:00" as the date and time 
+# When I select "December 25, 2008 10:00" as the date and time
 When /^I select "([^\"]*)" as the date and time$/ do |time|
   select_datetime(time)
 end
 
-# Use this step when using multiple datetime_select helpers on a page or 
+# Use this step when using multiple datetime_select helpers on a page or
 # you want to specify which datetime to select. Given the following view:
-#   <%= f.label :preferred %><br />
-#   <%= f.datetime_select :preferred %>
-#   <%= f.label :alternative %><br />
-#   <%= f.datetime_select :alternative %>
+# <%%= f.label :preferred %><br />
+# <%%= f.datetime_select :preferred %>
+# <%%= f.label :alternative %><br />
+# <%%= f.datetime_select :alternative %>
 # The following steps would fill out the form:
 # When I select "November 23, 2004 11:20" as the "Preferred" data and time
 # And I select "November 25, 2004 10:30" as the "Alternative" data and time
@@ -49,19 +49,19 @@ end
 # Use this step in conjunction with Rail's time_select helper. For example:
 # When I select "2:20PM" as the time
 # Note: Rail's default time helper provides 24-hour time-- not 12 hour time. Webrat
-# will convert the 2:20PM to 14:20 and then select it. 
+# will convert the 2:20PM to 14:20 and then select it.
 When /^I select "([^\"]*)" as the time$/ do |time|
   select_time(time)
 end
 
 # Use this step when using multiple time_select helpers on a page or you want to
-# specify the name of the time on the form.  For example:
+# specify the name of the time on the form. For example:
 # When I select "7:30AM" as the "Gym" time
 When /^I select "([^\"]*)" as the "([^\"]*)" time$/ do |time, time_label|
   select_time(time, :from => time_label)
 end
 
-# Use this step in conjunction with Rail's date_select helper.  For example:
+# Use this step in conjunction with Rail's date_select helper. For example:
 # When I select "February 20, 1981" as the date
 When /^I select "([^\"]*)" as the date$/ do |date|
   select_date(date)
@@ -75,11 +75,11 @@ When /^I select "([^\"]*)" as the "([^\"]*)" date$/ do |date, date_label|
 end
 
 When /^I check "([^\"]*)"$/ do |field|
-  check(field) 
+  check(field)
 end
 
 When /^I uncheck "([^\"]*)"$/ do |field|
-  uncheck(field) 
+  uncheck(field)
 end
 
 When /^I choose "([^\"]*)"$/ do |field|
@@ -101,3 +101,8 @@ end
 Then /^the "([^\"]*)" checkbox should be checked$/ do |label|
   field_labeled(label).should be_checked
 end
+
+Then /^I should be on (.+)$/ do |page_name|
+  URI.parse(current_url).path.should == path_to(page_name)
+end
+ 
