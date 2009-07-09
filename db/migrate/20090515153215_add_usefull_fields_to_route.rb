fd87f32701511057a93c02e78b2f75c20f5b01e1
Paco Guzman <pacoguzmanp@gmail.com>
2009-07-09 22:44:14 +0200

    Modifications. Change from string to text to save big encoded routes

--- db/migrate/20090515153215_add_usefull_fields_to_route.rb
+++ db/migrate/20090515153215_add_usefull_fields_to_route.rb
@@ -1,8 +1,8 @@
 class AddUsefullFieldsToRoute < ActiveRecord::Migration
   def self.up
     add_column :routes, :distance_unit, :string, :default => "km"
-    add_column :routes, :loops, :boolean, :default => false
-    add_column :routes, :encoded_points, :string, :default => ""
+    add_column :routes, :loops, :integer, :default => 1
+    add_column :routes, :encoded_points, :text, :default => ""
   end
 
   def self.down
